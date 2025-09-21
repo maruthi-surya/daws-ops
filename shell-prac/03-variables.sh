5ed8d2e07afa7183d5af5c2d615f661fc0eb6e5a
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-21 08:44:32 +0530

    another commit

---
+++ shell-prac/03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Joe
+PERSON2=Chris
+
+echo "$PERSON1:: how are you suresh"
+echo "$PERSON2:: I'm fine and how are you"
+echo "$PERSON1:: I'm also fine, what are you learing"
+echo "$PERSON2:: I'm learing shell scripting $PERSON1" 
--- shell-prac/multiline.sh
+++ shell-prac/multiline.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+echo "Rmesh:: how are you suresh"
+echo "Suresh:: I'm fine and how are you"
+echo "Ramesh:: I'm also fine, what are you learing"
+echo "Suresh:: I'm learing shell scripting"
