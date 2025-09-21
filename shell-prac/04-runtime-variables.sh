c9ad9dd9b1d030f430bad3f1b2878b46ea2bec57
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-21 09:05:27 +0530

    another commit

---
+++ shell-prac/04-runtime-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: how are you $PERSON2"
+echo "$PERSON2:: I'm fine and how are you"
+echo "$PERSON1:: I'm also fine, what are you learing"
+echo "$PERSON2:: I'm learing shell scripting $PERSON1" 
