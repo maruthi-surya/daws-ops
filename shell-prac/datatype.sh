cfb2067553af8c785da33add2e5353f4d873c01f
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-21 13:53:38 +0530

    another commit

--- shell-prac/datatype.sh
+++ shell-prac/datatype.sh
@@ -10,3 +10,11 @@ echo "Total is=${sum}"
 leaders=("Mike" "Tom" "Joe")
 
 echo "leaders are=${leaders[@]}"
+
+echo "leaders are=${leaders[@]}"
+
+echo "First Leader is=${leaders[0]}"
+
+echo "Second leader is=${leader[1]}"
+
+echo "Third Leader is=${leaders[2]}"
