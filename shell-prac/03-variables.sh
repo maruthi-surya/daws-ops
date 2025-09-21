d3d556937d3a2a95983be50e5ba949bff28290ed
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-21 08:46:43 +0530

    another commit

--- shell-prac/03-variables.sh
+++ shell-prac/03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Joe
 PERSON2=Chris
 
-echo "$PERSON1:: how are you suresh"
+echo "$PERSON1:: how are you $PERSON2"
 echo "$PERSON2:: I'm fine and how are you"
 echo "$PERSON1:: I'm also fine, what are you learing"
 echo "$PERSON2:: I'm learing shell scripting $PERSON1" 
