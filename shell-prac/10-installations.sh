9464d46c364118e520f0c49c621f5e8989d36400
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-22 16:00:25 +0530

    another commit

--- shell-prac/10-installations.sh
+++ shell-prac/10-installations.sh
@@ -3,23 +3,27 @@
 userid=$(id -u)
 
 if [ $userid -ne 0 ]; then
-
    echo "run with root user"
-
 exit 1
-
 fi
 
 dnf install mysql -y
 
 if [ $userid -ne 0 ]; then
-
    echo "run with sudo user"
-
 exit 1
 
 else
-
    echo "mysql install sucess"
+fi
+
+dnf install nginx -y
+
+if [ $userid -ne 0 ]; then
+   echo "run with root user"
+
+else
+  echo "inginx install success"
 
 fi
+  
