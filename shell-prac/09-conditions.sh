e31d4d062cad9a5650420bd4463bbe68bed27780
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-22 14:18:38 +0530

    another commit

---
+++ shell-prac/09-conditions.sh
@@ -0,0 +1,17 @@
+#!.bin/bash
+
+read number
+
+if [ $number -lt 10]; then
+
+  echo "The given number is less than 10"
+
+elif [ $number -eq 10]; then
+
+  echo "The given number is equal to 10"
+
+else
+
+  echo "The given number is greater than 10"
+
+fi 
