0e33828cae6262b9f4672838b4682fc424a28cd9
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-22 17:51:56 +0530

    another commit

---
+++ shell-prac/12-function.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+userid==$(id -u)
+
+if [ $userid -ne 0 ]; then
+   echo "ERROR:: Execute script with root priveleges"
+   exit 1
+fi
+
+dnf intall mysql -y
+
+if [ $? -ne 0 ]; then
+  echo "ERROR:: Mysql install failed "
+  exit 1
+else
+  echo "Mysql install success"
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+  echo "nginx install failed"
+  exit 1
+else
+  echo "nginx install sucess"
+
