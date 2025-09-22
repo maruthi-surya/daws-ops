d3096e5bb0fdf79ed1c0f09b1d1b78d25d39529a
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-22 14:20:04 +0530

    another commit

--- shell-prac/09-conditions.sh
+++ shell-prac/09-conditions.sh
@@ -1,12 +1,14 @@
 #!.bin/bash
 
+echo "Enter the number:"
+
 read number
 
-if [ $number -lt 10]; then
+if [ $number -lt 10 ]; then
 
   echo "The given number is less than 10"
 
-elif [ $number -eq 10]; then
+elif [ $number -eq 10 ]; then
 
   echo "The given number is equal to 10"
 
