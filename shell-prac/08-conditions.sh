f759f434468f749d3b4416c552b762df77fdfb13
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-22 14:14:39 +0530

    another commit

--- shell-prac/08-conditions.sh
+++ shell-prac/08-conditions.sh
@@ -4,14 +4,14 @@ number=$1
 
 if [ $number -lt 10 ]; then
 
-   echo "The given number is less than $number"
+   echo "The given number is less than 10"
 
 elif [ $number -eq 10 ]; then
 
-  echo "The given number is equal to $number"
+  echo "The given number is equal to 10"
 
 else
 
-  echo "The given number is greater than to $number"
+  echo "The given number is greater than to 10"
 
 fi   
