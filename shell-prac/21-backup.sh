9ac0b6a8e7abfc1d396d8c834c4388218c9024f6
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-26 14:34:51 +0530

    commit

--- shell-prac/21-backup.sh
+++ shell-prac/21-backup.sh
@@ -13,6 +13,7 @@ days=${3:-14} # if not provided considered as 14 days
 
 if [ $userid -ne 0 ]; then
    echo "run with sudo previleges"
+   exit 1
 fi
 log_dir=$(pwd) # log file directory
 curr_file=$( echo $0 | cut -d "." -f1) # spliing the file name with .
@@ -36,8 +37,8 @@ fi
 files=$( find $source_dir -name "*.log" -type f ) #-mtime +$days
 
 if [ ! -z "{$files}" ]; then
-   echo "no files present"
+   echo "files found"
 else
-  echo "files found"
+  echo "no files present"
 fi
 
