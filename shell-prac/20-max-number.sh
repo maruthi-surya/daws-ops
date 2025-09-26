4bcd4880b66974ede2c96823b5699403383bdc13
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-26 11:26:23 +0530

    commit

--- shell-prac/20-max-number.sh
+++ shell-prac/20-max-number.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-number=$@
+numbers=$@
 
-max_number=$(max[ $number ])
+# Initialize max with the first element of the array
+max=${numbers[0]}
 
-echo "Maximum number is: $max_number"
+# Iterate through the array starting from the second element
+for (( i=1; i<${#numbers[@]}; i++ )); do
+    current_number=${numbers[i]}
+    # Compare current_number with max
+    if (( current_number > max )); then
+        max=$current_number
+    fi
+done
+
+echo "The largest number in the array is: $max"
