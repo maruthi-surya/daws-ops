746e492ca1649088601a21ac783450eaa8691bc3
maruthi-surya <maruthi.surya.k@gmail.com>
2025-09-26 12:41:17 +0530

    commit

---
+++ shell-prac/21-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ userid -ne 0]
+   echo "run with sudo previleges"
+fi
+log_dir=$(pwd) # log file directory
+curr_file=$( echo $0 | cut -d "." -f1) # spliing the file name with .
+log_file="$log_dir/$curr_file.log" # log file name
